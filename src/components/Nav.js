6eb85068a3ea0a047767e019bc5e5804e7a8981e
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-11-17 21:00:15 -0700

    GH link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMusic } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>Waves</h1>
+      <h1>CBI
+        <a href={'https://github.com/Cristian-Baeza/MusicApp'} target={"_blank"}>
+          <FontAwesomeIcon
+            icon={faGithub}
+          />
+        </a>
+      </h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library
         <FontAwesomeIcon icon={faMusic} />
